996e317abb8d25abc8b004257c420efe39b6ba9b
Eduardo <144747945+eddchinea@users.noreply.github.com>
2023-10-25 13:22:21 -0400

    feat: create a playRound function
    We needed a function that take player's and computer's selections, and
    determine wich one is the winner

--- code.js
+++ code.js
@@ -5,10 +5,47 @@ function getComputerChoice() {
     const numChoice = Math.floor(Math.random() * 3 + 1)
 
     //Assign each number a choice following the name order (1: rock, 2: paper, 3: scissors)
-    if (numChoice == 1) choice = "rock"
-    else if (numChoice == 2) choice = "paper"
-    else choice = "scissors"
+    if (numChoice == 1) choice = 'rock'
+    else if (numChoice == 2) choice = 'paper'
+    else choice = 'scissors'
 
     //return the string value of choice (rock, paper or scissors)
     return choice
 }
+
+//Play a single round returning the winner and why
+function playRound(playerSelection, computerSelection) {
+    //Set a youWon variable to a boolean type
+    let youWon = true
+    //check if both selections are the same,
+    if (playerSelection == computerSelection) {
+        //And if they are the same, return a Tie result message
+        return `It's a tie! Both have chosen ${playerSelection}`
+    }
+    //if not, then determine if the player wins
+    else{
+        switch (playerSelection) {
+            case 'rock':
+                youWon = computerSelection == 'paper' ? false : true
+                break;
+
+            case 'paper':
+                youWon = computerSelection == 'scissors' ? false : true
+                break;
+
+            case 'scissors':
+                youWon = computerSelection == 'rock' ? false : true
+                break;
+        }
+    }
+
+    //if the player wins, return a winner message
+    if (youWon) return `You win! ${playerSelection} beats ${computerSelection}`
+    //if not, return a looser message
+    else return `You loose! ${computerSelection} beats ${playerSelection}`
+}
+
+let computerSelection = getComputerChoice()
+let playerSelection = prompt('What is your choice? Rock, Paper or Scissors?').toLowerCase()
+
+console.log(playRound(playerSelection, computerSelection))
